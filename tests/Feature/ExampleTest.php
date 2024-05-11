428e62d38953acc3283c6752a1947f3a343397f8
Jeremy McPeak <jwmcpeak@gmail.com>
2024-05-10 23:56:41 -0500

    Episode 7

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,6 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get('/');
-
-    $response->assertStatus(200);
+it('see Laracasts in welcome view', function () {
+    // $this->withoutVite();
+    $this->view('welcome')->assertSee('Laracasts');
 });
